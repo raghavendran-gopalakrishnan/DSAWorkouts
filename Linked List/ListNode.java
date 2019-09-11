3738e5ade96d5810d33103565a3eed60a9b3c394
Raghavendran Gopalakrishnan <raghavendran.gopalakrishnan@nokia.com>
2019-09-11 18:59:04 +0530

    Create ListNode.java

---
+++ Linked List/ListNode.java
@@ -0,0 +1,19 @@
+public class ListNode{
+    private int data;
+    private ListNode next;
+    public ListNode(int data){
+        this.data = data;
+    }
+    public setData(int data){
+        this.data = data;
+    }
+    public int getData(){
+        return data;
+    }
+    public setNext(LinkNode next){
+        this.next = next;
+    }
+    public ListNode getNext(){
+        return next;
+    }
+}
